b4b996e3dd5fd110aaba8a9796dbf25566a554f0
theredfish <did.julian@gmail.com>
2024-03-21 00:08:22 +0100

    Bug demo with animated fox example

--- examples/animation/animated_fox.rs
+++ examples/animation/animated_fox.rs
@@ -135,79 +135,78 @@ fn keyboard_animation_control(
     mut current_animation: Local<usize>,
 ) {
     for (mut player, mut transitions) in &mut animation_players {
-        let Some((&playing_animation_index, _)) = player.playing_animations().next() else {
-            continue;
-        };
+        // let Some((&playing_animation_index, _)) = player.playing_animations().next() else {
+        //     continue;
+        // };
 
-        if keyboard_input.just_pressed(KeyCode::Space) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            if playing_animation.is_paused() {
-                playing_animation.resume();
-            } else {
-                playing_animation.pause();
-            }
-        }
+        // if keyboard_input.just_pressed(KeyCode::Space) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     if playing_animation.is_paused() {
+        //         playing_animation.resume();
+        //     } else {
+        //         playing_animation.pause();
+        //     }
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::ArrowUp) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            let speed = playing_animation.speed();
-            playing_animation.set_speed(speed * 1.2);
-        }
+        // if keyboard_input.just_pressed(KeyCode::ArrowUp) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     let speed = playing_animation.speed();
+        //     playing_animation.set_speed(speed * 1.2);
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::ArrowDown) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            let speed = playing_animation.speed();
-            playing_animation.set_speed(speed * 0.8);
-        }
+        // if keyboard_input.just_pressed(KeyCode::ArrowDown) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     let speed = playing_animation.speed();
+        //     playing_animation.set_speed(speed * 0.8);
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::ArrowLeft) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            let elapsed = playing_animation.seek_time();
-            playing_animation.seek_to(elapsed - 0.1);
-        }
+        // if keyboard_input.just_pressed(KeyCode::ArrowLeft) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     let elapsed = playing_animation.seek_time();
+        //     playing_animation.seek_to(elapsed - 0.1);
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::ArrowRight) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            let elapsed = playing_animation.seek_time();
-            playing_animation.seek_to(elapsed + 0.1);
-        }
+        // if keyboard_input.just_pressed(KeyCode::ArrowRight) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     let elapsed = playing_animation.seek_time();
+        //     playing_animation.seek_to(elapsed + 0.1);
+        // }
 
         if keyboard_input.just_pressed(KeyCode::Enter) {
-            *current_animation = (*current_animation + 1) % animations.animations.len();
+            *current_animation = 2;
 
             transitions
                 .play(
                     &mut player,
                     animations.animations[*current_animation],
                     Duration::from_millis(250),
-                )
-                .repeat();
+                );
         }
 
-        if keyboard_input.just_pressed(KeyCode::Digit1) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            playing_animation
-                .set_repeat(RepeatAnimation::Count(1))
-                .replay();
-        }
+        // if keyboard_input.just_pressed(KeyCode::Digit1) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     playing_animation
+        //         .set_repeat(RepeatAnimation::Count(1))
+        //         .replay();
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::Digit3) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            playing_animation
-                .set_repeat(RepeatAnimation::Count(3))
-                .replay();
-        }
+        // if keyboard_input.just_pressed(KeyCode::Digit3) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     playing_animation
+        //         .set_repeat(RepeatAnimation::Count(3))
+        //         .replay();
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::Digit5) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            playing_animation
-                .set_repeat(RepeatAnimation::Count(5))
-                .replay();
-        }
+        // if keyboard_input.just_pressed(KeyCode::Digit5) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     playing_animation
+        //         .set_repeat(RepeatAnimation::Count(5))
+        //         .replay();
+        // }
 
-        if keyboard_input.just_pressed(KeyCode::KeyL) {
-            let playing_animation = player.animation_mut(playing_animation_index).unwrap();
-            playing_animation.set_repeat(RepeatAnimation::Forever);
-        }
+        // if keyboard_input.just_pressed(KeyCode::KeyL) {
+        //     let playing_animation = player.animation_mut(playing_animation_index).unwrap();
+        //     playing_animation.set_repeat(RepeatAnimation::Forever);
+        // }
     }
 }
